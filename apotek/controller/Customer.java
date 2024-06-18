6f7c18880af25d4e324fede902149ea48d096283
Difeb24 <dwifebri2324@gmail.com>
2024-06-18 20:30:04 +0800

    220010138

---
+++ apotek/controller/Customer.java
@@ -0,0 +1,7 @@
+package apotek.controller;
+
+public class Customer extends User{
+    public Customer(String nama, String alamat, String email, String hp) {
+        super(nama, alamat, email, hp);
+    }
+}
