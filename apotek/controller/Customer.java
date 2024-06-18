c688885a585394968d45b8beec8b1e8b0a8a53e2
Difeb24 <dwifebri2324@gmail.com>
2024-06-18 20:29:03 +0800

    Delete apotek/controller/Customer.java

--- apotek/controller/Customer.java
+++
@@ -1,7 +0,0 @@
-package apotek.controller;
-
-public class Customer extends User{
-    public Customer(String nama, String alamat, String email, String hp) {
-        super(nama, alamat, email, hp);
-    }
-}
